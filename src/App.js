0cacaad86bb97dd6647ca6bb14413d5ee1bfc794
sakura miyamoto <sakura.m.208@gmail.com>
2022-09-19 14:32:18 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React, { useState } from 'react'
 import TopImg from "./img/top.png";
 import AppleIcon from "./img/dl_icon_apple.png";
 import GoogleIcon from "./img/dl_icon_google.png";
-import Title1 from "./img/title1.png";
-import Title2 from "./img/title2.png";
-import Title3 from "./img/title3.png";
-import Title4 from "./img/title4.png";
-import Title5 from "./img/title5_news.png";
 import Point1 from "./img/canIcon/1.png";
 import Point2 from "./img/canIcon/2.png";
 import Point3 from "./img/canIcon/3.png";
@@ -21,28 +16,26 @@ import tag3 from "./img/icon/59xxxhdpi.png";
 import tag4 from "./img/icon/60xxxhdpi.png";
 import Contents3 from "./img/title3_contents.png";
 import Contents4 from "./img/title4_contents.png";
-import NewsContents from "./img/title5_news_contents.png";
-import Title6 from "./img/title6_contact.png";
 import ContactButton from "./img/title6_contact_contents2.png";
 import Bottom from "./img/bottom.png";
 import mov1 from "./img/movie/a.mp4";
 import mov2 from "./img/movie/b.mp4";
 import mov3 from "./img/movie/c.mp4";
 import mov4 from "./img/movie/d.mp4";
+import ajian from "./img/dish-icon/icon_ajian.png";
+import cafe from "./img/dish-icon/icon_cafe.png";
+import fastfood from "./img/dish-icon/icon_fastfood.png";
+import gohan from "./img/dish-icon/icon_gohan.png";
+import men from "./img/dish-icon/icon_men.png";
+import niku from "./img/dish-icon/icon_niku.png";
+import nomiya from "./img/dish-icon/icon_nomiya.png";
+import sakana from "./img/dish-icon/icon_sakana.png";
+import washoku from "./img/dish-icon/icon_washoku.png";
+import youshoku from "./img/dish-icon/icon_youshoku.png";
+
 import './App.scss';
 
 function App() {
-  // const videoRef = useRef(null);
-  // const videoRef1 = useRef(null);
-  // const videoRef2 = useRef(null);
-  // const videoRef3 = useRef(null);
-  //   useEffect(() => {
-  //       videoRef.current?.play();
-  //       videoRef1.current?.play();
-  //       videoRef2.current?.play();
-  //       videoRef3.current?.play();
-  //   }, []);
-
   const [mailBody, setMailBody] = useState('')
   const [mailName, setMailName] = useState('')
 
@@ -69,7 +62,7 @@ function App() {
 
       {/*  Popdish とは */}
       <div className="about">
-        <img src={Title1} className="title" alt="logo" />
+          <h1><span>Popdish(ポップディッシュ)とは？</span></h1>
           <p>Popdishとは、動画と地図でグルメ情報を投稿検索でき、</p>
           <p>気になるお店情報の保存や、好みが近いユーザーのフォローもできる、</p>
           <p>新感覚のグルメ動画SNS。</p>
@@ -79,7 +72,7 @@ function App() {
 
       {/*  Popdish で何ができるの */}
         <div className="why-popdish">
-          <img src={Title2} className="title" alt="logo" />
+          <h2><span>Popdish で何ができるの?</span></h2>
           <div className="img-row">
             <div>
               <img src={Point1} alt="logo" />
@@ -151,23 +144,42 @@ function App() {
 
         {/*  Popdish で探せる料理ジャンル一覧 */}
         <div className="genre">
-          <img src={Title3} className="title" alt="logo" />
+          <h2><span>Popdish で探せる料理ジャンル一覧</span></h2>
           <img src={Contents3} className="contents" alt="logo" />
+          {/* <div className="contents">
+            <div className="row">
+              <div className="genre-div">
+                <img src={niku} className="title" alt="logo" />
+                <p>
+                  肉料理
+                </p>
+              </div>
+            </div>
+          </div> */}
         </div>
 
       {/*  Popdish で何ができるの */}
         <div className="can">
-          <img src={Title4} className="title" alt="logo" />
+          <h2><span>Popdishはこんな人にオススメ！</span></h2>
           <img src={Contents4} className="contents" alt="logo" />
         </div>
 
       {/*  NEWS */}
         <div className="news">
-          <img src={Title5} className="title" alt="logo" />
-          <img src={NewsContents} className="contents" alt="logo" />
+                    <h2><span>NEWS</span></h2>
+          <ul>
+            <li>
+              2022年7月　日本スタートアップ支援２号投資事業有限責任組合から、J-KISS型新株予約権による資金調達を実施しました。
+            </li>
+            <li>2022年7月　Androidアプリのベータ版をリリースしました。</li>
+            <li>2022年6月　iPhoneアプリのベータ版をリリースしました。</li>
+            <li>2022年3月　JSSA（日本スタートアップ支援協会）主催のピッチコンテストで、オーディエンス賞、スポンサー賞（第一法律事務所賞）をダブル受賞しました。</li>
+            <li>2021年11月　G'sAcademy主催のピッチコンテストGLOBAL GEEK AUDITIONにて優勝しました。</li>
+            <li>2021年9月　経済産業省・JETRO（独立行政法人日本貿易振興機構）主催、グローバル企業家等育成プログラム「始動Next Innovator 2021」に採択されました。</li>
+          </ul>
         </div>
 
-             <div className="bottom-icon">
+        <div className="bottom-icon">
           <a href="https://apps.apple.com/us/app/popdish/id1618467447?itsct=apps_box_badge&amp;itscg=30200">
             <img src={AppleIcon} className="dl-icon-apple" alt="logo" />
           </a>
@@ -177,7 +189,7 @@ function App() {
         </div>
 
         <div className="contact">
-          <img src={Title6} className="title" alt="logo" />
+          <h2><span>CONTACT</span></h2>
           <div className="contact-input">
             <p>NAME<span>*</span></p>
             <input type="text" onChange={(e) => setMailName(e.target.value)} />
@@ -192,17 +204,12 @@ function App() {
       </div>
 
       <div className="bottom">
-
-        <div >
-
-        <a href="https://meshi-ran-kiyaku.web.app/">利用規約</a>
-          <a href="https://nobuplanning.com">運営会社</a>
-          <p>©︎NOBU Planning Inc. 2022
-</p>
-        </div>
-
         <img src={Bottom} className="bottom-bg" alt="logo" />
-
+        <div>
+          <a href="https://meshi-ran-kiyaku.web.app/">利用規約</a>
+          <a href="https://nobuplanning.com">運営会社</a>
+          <p>©︎NOBU Planning Inc. 2022</p>
+        </div>
       </div>
 
     </div>
